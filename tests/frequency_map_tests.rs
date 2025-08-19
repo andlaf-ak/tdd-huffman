c76a6a536d6c9a7f12628239b9ef7ad7a43b0847
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-19 16:55:36 +0200

    Red phase: count_multiple_occurrences_of_same_byte

--- tests/frequency_map_tests.rs
+++ tests/frequency_map_tests.rs
@@ -6,3 +6,10 @@ fn count_single_byte_occurrence() {
     let frequencies = count_byte_frequencies(&input);
     assert_eq!(frequencies.get(&65), Some(&1));
 }
+
+#[test]
+fn count_multiple_occurrences_of_same_byte() {
+    let input = [65u8, 65u8, 65u8];
+    let frequencies = count_byte_frequencies(&input);
+    assert_eq!(frequencies.get(&65), Some(&3));
+}
