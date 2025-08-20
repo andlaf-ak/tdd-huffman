8275b03bbded1424083dab8e8fa3eeb8b29f2d63
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-20 18:36:03 +0200

    Fix format

--- tests/tree_construction_tests.rs
+++ tests/tree_construction_tests.rs
@@ -2,11 +2,11 @@ use tdd_huffman::merge_leaf_nodes;
 
 #[test]
 fn merge_two_leaf_nodes() {
-    let left_node = (65u8, 3usize);  // 'A' with frequency 3
+    let left_node = (65u8, 3usize); // 'A' with frequency 3
     let right_node = (66u8, 2usize); // 'B' with frequency 2
-    
+
     let merged_node = merge_leaf_nodes(left_node, right_node);
-    
+
     // The merged node should have combined frequency (3 + 2 = 5)
     // and should contain references to both child nodes
     assert_eq!(merged_node.frequency(), 5);
