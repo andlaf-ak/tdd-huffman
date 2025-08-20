7314885d1b16ea26191fc7afa6d54231ec724965
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-20 18:37:27 +0200

    Green phase: merge_two_leaf_nodes

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,9 @@
 
 pub mod frequency_map;
 pub mod node_selection;
+pub mod tree_construction;
 
 // Re-export main functions and types for easier access
 pub use frequency_map::{count_byte_frequencies, ByteFrequencyMap};
 pub use node_selection::{select_nodes, NodeCollection, SymbolFrequency};
+pub use tree_construction::{merge_leaf_nodes, HuffmanNode};
---
+++ src/tree_construction.rs
@@ -0,0 +1,36 @@
+pub type SymbolFrequency = (u8, usize);
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct HuffmanNode {
+    frequency: usize,
+    symbol: Option<u8>,
+    left_child: Option<SymbolFrequency>,
+    right_child: Option<SymbolFrequency>,
+}
+
+impl HuffmanNode {
+    pub fn frequency(&self) -> usize {
+        self.frequency
+    }
+
+    pub fn symbol(&self) -> Option<u8> {
+        self.symbol
+    }
+
+    pub fn left_child(&self) -> Option<&SymbolFrequency> {
+        self.left_child.as_ref()
+    }
+
+    pub fn right_child(&self) -> Option<&SymbolFrequency> {
+        self.right_child.as_ref()
+    }
+}
+
+pub fn merge_leaf_nodes(left: SymbolFrequency, right: SymbolFrequency) -> HuffmanNode {
+    HuffmanNode {
+        frequency: left.1 + right.1,
+        symbol: None,
+        left_child: Some(left),
+        right_child: Some(right),
+    }
+}
