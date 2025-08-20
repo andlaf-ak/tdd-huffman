b37a1c14fef10bbd99b590a88135904cabb7ea21
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-20 15:16:47 +0200

    Fix format

--- tests/node_selection_tests.rs
+++ tests/node_selection_tests.rs
@@ -4,7 +4,7 @@ use tdd_huffman::select_nodes;
 fn select_single_node_when_only_one_symbol_exists() {
     let input = [(65u8, 1usize)]; // Single symbol 'A' with frequency 1
     let selected_nodes = select_nodes(&input);
-    
+
     assert_eq!(selected_nodes.len(), 1);
     // The single node should contain the symbol and its frequency
     assert!(selected_nodes.contains(&(65u8, 1usize)));
