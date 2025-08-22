8d6e8ffb13327252bb55f2e1c98a749a9b340b56
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-22 12:24:06 +0200

    Refactor: write_more_than_8_bits_and_verify_multiple_byte_emissions

--- src/bit_stream.rs
+++ src/bit_stream.rs
@@ -4,6 +4,8 @@ pub struct BitStream<W> {
     bits_in_current_byte: usize,
 }
 
+const BITS_PER_BYTE: usize = 8;
+
 impl<W: std::io::Write> BitStream<W> {
     pub fn new(writer: W) -> Self {
         Self {
@@ -13,21 +15,26 @@ impl<W: std::io::Write> BitStream<W> {
         }
     }
 
+    fn emit_current_byte(&mut self) -> std::io::Result<()> {
+        self.writer.write_all(&[self.current_byte])?;
+        self.current_byte = 0;
+        self.bits_in_current_byte = 0;
+        Ok(())
+    }
+
     pub fn write_bit(&mut self, bit: u8) -> std::io::Result<()> {
         debug_assert!(bit <= 1, "Bit value must be 0 or 1, got: {bit}");
 
         // Set the bit in the current byte (from left to right)
         if bit == 1 {
-            self.current_byte |= 1 << (7 - self.bits_in_current_byte);
+            self.current_byte |= 1 << (BITS_PER_BYTE - 1 - self.bits_in_current_byte);
         }
 
         self.bits_in_current_byte += 1;
 
         // Emit byte to output stream when we have 8 bits, then reset
-        if self.bits_in_current_byte == 8 {
-            self.writer.write_all(&[self.current_byte])?;
-            self.current_byte = 0;
-            self.bits_in_current_byte = 0;
+        if self.bits_in_current_byte == BITS_PER_BYTE {
+            self.emit_current_byte()?;
         }
 
         Ok(())
@@ -36,11 +43,7 @@ impl<W: std::io::Write> BitStream<W> {
     pub fn flush(&mut self) -> std::io::Result<()> {
         // If we have any pending bits, emit them as a padded byte
         if self.bits_in_current_byte > 0 {
-            // The remaining bits are already in the correct positions (left-aligned)
-            // Zero bits are automatically in the right positions since we start with 0
-            self.writer.write_all(&[self.current_byte])?;
-            self.current_byte = 0;
-            self.bits_in_current_byte = 0;
+            self.emit_current_byte()?;
         }
         Ok(())
     }
