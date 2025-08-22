d4d0926f54142a4cc43b3e9f70369b0a8b514e3a
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-22 15:50:57 +0200

    Red phase: single_leaf_tree_serializes_to_1_followed_by_symbol

---
+++ tests/unit/tree_serialization_tests.rs
@@ -0,0 +1,18 @@
+use tdd_huffman::HuffmanNode;
+
+#[test]
+fn single_leaf_tree_serializes_to_1_followed_by_symbol() {
+    // Arrange: Create a single leaf tree with symbol 65 (ASCII 'A')
+    let tree = HuffmanNode::new_leaf(65u8, 5);
+    
+    // Act: Serialize the tree
+    let result = serialize_tree(&tree);
+    
+    // Assert: Should be "1" + 8-bit representation of 65 = "101000001"
+    assert_eq!(result, "101000001");
+}
+
+// Function that doesn't exist yet - this will cause compilation to fail (RED phase)
+fn serialize_tree(tree: &HuffmanNode) -> String {
+    unimplemented!("Tree serialization not yet implemented")
+}
