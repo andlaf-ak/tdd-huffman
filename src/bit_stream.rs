3fdf739f85825090ac44b6e807273047b7fe48df
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-22 11:37:19 +0200

    Refactor: write_multiple_individual_bits_in_sequence

--- src/bit_stream.rs
+++ src/bit_stream.rs
@@ -1,5 +1,4 @@
 pub struct BitStream {
-    bit_count: usize,
     bits: Vec<u8>,
 }
 
@@ -11,23 +10,19 @@ impl Default for BitStream {
 
 impl BitStream {
     pub fn new() -> Self {
-        Self { 
-            bit_count: 0,
-            bits: Vec::new(),
-        }
+        Self { bits: Vec::new() }
     }
 
     pub fn write_bit(&mut self, bit: u8) {
         debug_assert!(bit <= 1, "Bit value must be 0 or 1, got: {bit}");
         self.bits.push(bit);
-        self.bit_count += 1;
     }
 
     pub fn bit_count(&self) -> usize {
-        self.bit_count
+        self.bits.len()
     }
 
-    pub fn get_bits(&self) -> Vec<u8> {
-        self.bits.clone()
+    pub fn get_bits(&self) -> &[u8] {
+        &self.bits
     }
 }
