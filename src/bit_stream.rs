728651096d8d6fd162b79169c8cf7e7489d95c40
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-08-22 11:35:39 +0200

    Green phase: write_multiple_individual_bits_in_sequence

--- src/bit_stream.rs
+++ src/bit_stream.rs
@@ -1,5 +1,6 @@
 pub struct BitStream {
     bit_count: usize,
+    bits: Vec<u8>,
 }
 
 impl Default for BitStream {
@@ -10,15 +11,23 @@ impl Default for BitStream {
 
 impl BitStream {
     pub fn new() -> Self {
-        Self { bit_count: 0 }
+        Self { 
+            bit_count: 0,
+            bits: Vec::new(),
+        }
     }
 
     pub fn write_bit(&mut self, bit: u8) {
         debug_assert!(bit <= 1, "Bit value must be 0 or 1, got: {bit}");
+        self.bits.push(bit);
         self.bit_count += 1;
     }
 
     pub fn bit_count(&self) -> usize {
         self.bit_count
+    }
+
+    pub fn get_bits(&self) -> Vec<u8> {
+        self.bits.clone()
     }
 }
