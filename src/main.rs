94b160d7e7bc7375d7d405df8d3925aa39f24e13
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-09-03 10:38:28 +0100

    Add reminders

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
     println!("TDD Huffman Compression");
+    // make sure that files with a size that cannot be stored in 4 bytes are rejected
+    // remove the "examples" directoy
 }
