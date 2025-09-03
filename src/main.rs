c6191e802447e392c8b4775d952fc480da16ec83
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-09-03 14:17:30 +0100

    Add TODO

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ fn main() {
     println!("TDD Huffman Compression");
     // make sure that files with a size that cannot be stored in 4 bytes are rejected
     // remove the "examples" directoy
+    // check if the way tests are structured is aligned to Rust standards
 }
