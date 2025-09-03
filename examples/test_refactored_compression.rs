7bacaf09e6a7d113b8f32d4bc81ec9f538686bdf
Andrea Laforgia <andrea.laforgia@armakuni.com>
2025-09-03 17:25:11 +0100

    Use longer strings as sample inputs

--- examples/test_refactored_compression.rs
+++ examples/test_refactored_compression.rs
@@ -2,7 +2,7 @@ use std::io::Cursor;
 use tdd_huffman::compress;
 
 fn main() {
-    let test_string = "hello world";
+    let test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.";
     let mut compressed_data = Vec::new();
 
     compress(Cursor::new(test_string.as_bytes()), &mut compressed_data)
